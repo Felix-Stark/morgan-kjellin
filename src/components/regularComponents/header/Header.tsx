9591a3f0764620cfba6afbaf3ef5f2605d0da207
Christopher Wahlström <83230387+christopherwahlstrom@users.noreply.github.com>
2023-04-26 14:23:50 +0200

    header design and link to correct route working

--- src/components/regularComponents/header/Header.tsx
+++ src/components/regularComponents/header/Header.tsx
@@ -2,18 +2,27 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton, { Container }  from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import Container from "@mui/material/Container";
 import Typography from '@mui/material/Typography';
-import Menu from "@mui/material";
+import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material";
-import MenuItem from "@mui/material";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
 import { Link } from "react-router-dom";
 
 
-const pagesLeft = [ "STARTSIDA", "OM MIG", "ARBETA MED MIG" ];
-const pagesRight = [ "BLOG", "KONTAKT", "BOKA MÖTE" ];
+const pagesLeft = [ 
+  {label: "STARTSIDA", path: "/"},
+  {label: "OM MIG", path: "/about"},
+  {label: "ARBETA MED MIG", path: "/arbeta-med-mig"},
+  ];
+const pagesRight = [ 
+  // {label: "BLOGG", path: "/blog"},
+  {label: "KONTAKT", path: "/contact"},
+  // {label: "BOKA MÖTE", path: "/meeting"},
+];
 
 export const Header = () => {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
@@ -30,42 +39,80 @@ export const Header = () => {
 
 
   return (
-    <AppBar position="static" sx={{ background: "#F6F6F6",  }}>
+    <AppBar position="static" sx={{ background: "#F6F6F6", width: "100vw"  }}>
       <Container maxWidth="lg">
         <Toolbar>
+        <Box sx={{ display: { xs: "flex", md: "none", flexBasis: "" } , marginRight: "auto"}}>
+            <Tooltip title="Öppna meny">
+              <IconButton
+                size="large"
+                edge="end"
+                color="inherit"
+                aria-label="Öppna meny"
+                onClick={handleOpenNavMenu}
+              >
+                <MenuIcon sx={{color: "#BA1D37" }} />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              id="nav-menu"
+              anchorEl={anchorElNav}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+            >
+              {pagesLeft.map((pageL) => (
+                <MenuItem
+                  key={pageL.label}
+                  onClick={handleCloseNavMenu}
+                  component={Link}
+                  to={pageL.path}
+                >
+                  {pageL.label}
+                </MenuItem>
+              ))}
+              {pagesRight.map((pageR) => (
+                <MenuItem
+                  key={pageR.label}
+                  onClick={handleCloseNavMenu}
+                  component={Link}
+                  to={pageR.path}
+                >
+                  {pageR.label}
+                </MenuItem>
+              ))}
+            </Menu>
+            </Box>
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
             {pagesLeft.map((pageL) => (
               <Button
-                key={pageL}
+                key={pageL.label}
                 color="inherit"
                 sx={{ textTransform: "none", color: "#BA1D37", fontWeight: "bold" }}
                 component={Link}
-                to={`/${pageL}`}
+                to={pageL.path}
               >
-                {pageL}
+                {pageL.label}
               </Button>
             ))}
           </Box>
-          {/* <Box sx={{ display:"flex"  }}> */}
             <Link to="/" style={{ textDecoration: "none",display: "flex" ,flexDirection: "row"  }}>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: "#BA1D37", fontWeight: "700", fontSize: 36 }}>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: "#BA1D37", fontWeight: "700", fontSize: 30 }}>
                 MORGAN&nbsp; 
               </Typography>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1, color:"#1E1E1E", fontWeight: "700", fontSize: 36 }}>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1, color:"#1E1E1E", fontWeight: "700", fontSize: 30 }}>
                 KJELLIN
               </Typography>
             </Link>
-          {/* </Box> */}
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
             {pagesRight.map((pageR) => (
               <Button
-                key={pageR}
+                key={pageR.label}
                 color="inherit"
                 sx={{ textTransform: "none", color: "#1E1E1E", fontWeight: "bold" }}
                 component={Link}
-                to={`/${pageR}`}
+                to={pageR.path}
               >
-                {pageR}
+                {pageR.label}
               </Button>
             ))}
           </Box>
