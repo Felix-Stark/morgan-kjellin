7726d029d67b95084a88cd5aef273e2862f6a092
Zeeroon-e <chia.kmohamad@hotmail.com>
2023-04-26 20:30:06 +0200

    cleaned up code

--- src/Views/About/About.tsx
+++ src/Views/About/About.tsx
@@ -5,39 +5,19 @@ import testP from '../../assets/profile-photo.jpg'
 export const About = () => {
   return <Container sx={{   width: {xs: 310, md: "90vw" },  padding: '0'}}>
 
-    <Box  sx={{ boxShadow: 3 , display: 'flex', marginBottom:"5vh",flexDirection:{ xs: "column-reverse", md: "row"}, justifyContent: 'space-between'}}>
-      <Paper  sx={{ borderRadius:'0px', color:"white", bgcolor:"#342C39"}}>
+    <Box sx={{ boxShadow: 3 , display: 'flex', marginBottom:"5vh",flexDirection:{ xs: "column-reverse", md: "row"}, justifyContent: 'space-between'}}>
+      <Paper sx={{ borderRadius:'0px', color:"white", bgcolor:"#342C39"}}>
         <Typography variant="h4" sx={{  marginBottom:"1rem"}}>Morgan Kjellin</Typography>
         <Typography variant="h6" sx={{ fontSize: '1em', margin: "auto", padding:"1rem"}}>
-        Jag är föreläsare & entreprenör och har sedan 1995 delat med mig av min resa. 
-        Med mina egna erfarenheter av att ändra mina tankar och mitt sätt att vara på, 
-        så har jag kunnat hjälpa många andra personer.
+          Jag är föreläsare & entreprenör och har sedan 1995 delat med mig av min resa. 
+          Med mina egna erfarenheter av att ändra mina tankar och mitt sätt att vara på, 
+          så har jag kunnat hjälpa många andra personer.
         </Typography>
       </Paper>
-      <Box
-        component="img"
-        sx={{
-          width: 310,
-          bgcolor:'gray',
-          maxWidth: { xs: 310, md: 250 },
-        }}
-        alt="bild"
-        src={testP}
-      />
+      <Box component="img"sx={{bgcolor:'gray',maxWidth: { xs: 310, md: 250 },}} alt="bild"src={testP}/>
     </Box>
     <Box sx={{boxShadow: 3 , display: 'flex', marginBottom:"5vh",  flexDirection:{ xs: "column", md: "row"}, justifyContent: 'space-between'}}>
-      <Box
-          component="img"
-          sx={{
-            
-            width: 310,
-            bgcolor:'gray',
-            maxWidth: { xs: 310, md: 250 },
-          }}
-          alt="bild"
-          src={testP}
-        />
-      
+      <Box component="img"sx={{bgcolor:'gray',maxWidth: { xs: 310, md: 250 },}}alt="bild"src={testP}/>
       <Paper  sx={{ borderRadius:'0px', bgcolor:"#342C39", color:"white"}}>
         <Typography variant="h4" sx={{  marginBottom:"1rem"}}>Före detta missbrukare</Typography>
         <Typography variant="h6" sx={{ fontSize: '1em', margin: "auto", padding:"1rem"}}>
@@ -46,27 +26,19 @@ export const About = () => {
           hur han blev avskedad från jobb och om goda relationer som gick förlorade under dessa år.
           Vännerna försökte förgäves att få honom att själv inse att han var illa ute.
           De sista åren kändes som att leva på en soptipp och det tog många år innan han kunde ta steget och få hjälp genom ett behandlingshem.
-
         </Typography>
       </Paper>
     </Box>
     <Paper elevation={3} sx={{padding: "1rem", borderRadius:'0px', color:"white", bgcolor:"#342C39"}}>
       <Typography variant="h4" sx={{  marginBottom:"1rem"}}>Morgans egna ord</Typography>
       <Typography variant="h6" sx={{ fontSize: '1em', margin: "auto"}}>
-      
-
         “Mina erfarenheter har fått mig att växa som människa. 
         Jag vill dela med mig av historien om vägen ur mitt missbruk och berätta om vilka verktyg och metoder som har hjälpt mig, så att jag nu kan hjälpa andra. 
         Jag borde många gånger ha tänkt först och gjort sen, men det gjorde jag inte då. Idag lever jag med eftertänksamhet och reflektion. 
         Jag tar hand om mig själv och för mig handlade det om att förändra sina vanor. Det är väldigt färdefulla erfarenheter och metoder jag vill dela med mig av.
         Förändring är möjligt för alla och det jag lärt mig har hjälpt mig att vara fortsatt drogfri även genom de kriser jag upplevt efter att jag blivit drogfri.”
-
        <Typography variant="h6" sx={{  marginBottom:"1rem", marginTop: "1rem"}}>“FÖRÄNDRING ÄR MÖJLIGT FÖR ALLA!”</Typography>
       </Typography>
-
-    </Paper>
-      
-    
-    
+    </Paper> 
   </Container>
 };
