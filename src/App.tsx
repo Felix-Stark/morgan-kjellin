48ee4669a0739e97ee77795f7e2c696201d3260b
Christopher Wahlström <83230387+christopherwahlstrom@users.noreply.github.com>
2023-04-27 18:45:41 +0200

    footer , links and most of design check

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components/regularComponents/header/Header";
+import { Footer } from "./components/regularComponents/footer/Footer";
 import { About } from "./Views/About/About";
 import { Contact } from "./Views/Contact/Contact";
 import { Home } from "./Views/Home/Home";
 import { Work } from "./Views/Work/Work";
 import { Blog } from "./Views/Blog/Blog";
+
+
 
 function App() {
      
@@ -21,6 +24,7 @@ function App() {
         <Route path="/arbeta-med-mig" element={<Work />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
