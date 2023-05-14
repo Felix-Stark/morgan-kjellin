32f74a97cfa0a219da7bcc3c78ba11c25106157a
Zeeroon-e <chia.kmohamad@hotmail.com>
2023-05-14 02:17:29 +0200

    added some styling

--- src/Views/Admin/UpdateText.tsx
+++ src/Views/Admin/UpdateText.tsx
@@ -13,21 +13,21 @@ const  UpdateText=() => {
         <Grid item xs={12} sx={{display:"flex", justifyContent:"center", borderBottom:"1px solid white", padding:"2rem"}}>
           <Typography variant="h3" color="white">Välj en text att uppdatera</Typography>
         </Grid>
-        <Grid xs={10} sx={{display:"flex", justifyContent:"space-between",textAlign:"center"}}>
+        <Grid xs={10} sx={{display:"flex", justifyContent:"space-between",textAlign:"center", marginBottom:"1vh"}}>
           <Grid item xs={3}>
-            <Typography variant="h4" color="white" >Titel |</Typography>  
+            <Typography variant="h4" color="white"  sx={{border:"1px solid white"}}>Titel </Typography>  
           </Grid>
           <Grid item xs={3}>
-            <Typography variant="h4" color="white">Innehåll |</Typography>  
+            <Typography variant="h4" color="white" sx={{border:"1px solid white"}}>Innehåll</Typography>  
           </Grid>
           <Grid item xs={3}>
-            <Typography variant="h4" color="white">Plats |</Typography>  
+            <Typography variant="h4" color="white" sx={{border:"1px solid white"}}>Plats</Typography>  
           </Grid>
           <Grid item xs={3}>
              
           </Grid>
         </Grid>
-        <Grid xs={10} sx={{display:"flex",justifyContent:"center",alignItems:"center",textAlign:"center" ,bgcolor:"gray", margin:"1rem"}}>
+        <Grid xs={10} sx={{display:"flex",justifyContent:"center",alignItems:"center",textAlign:"center" ,bgcolor:"gray", marginBottom:"1rem"}}>
           <Grid item xs={3}>
             <Typography  variant="body1" color="white">Relationer</Typography>  
           </Grid>
@@ -40,7 +40,21 @@ const  UpdateText=() => {
           <Grid item xs={3}>
             <Box component="img" alt="bild"src={editIcon} sx={{'&:hover': { cursor:"pointer", width:"1.5vw"}}}/>
           </Grid>
-        </Grid>  
+        </Grid>
+        <Grid xs={10} sx={{display:"flex",justifyContent:"center",alignItems:"center",textAlign:"center" ,bgcolor:"gray", marginBottom:"1rem"}}>
+          <Grid item xs={3}>
+            <Typography  variant="body1" color="white">Relationer</Typography>  
+          </Grid>
+          <Grid item xs={3}>
+            <Typography variant="body1" color="white">Lorem ipsum dolor sit amet magna aliqua.</Typography>
+          </Grid>
+          <Grid item xs={3}>
+            <Typography variant="body1" color="white">Arbeta med mig</Typography>
+          </Grid>
+          <Grid item xs={3}>
+            <Box component="img" alt="bild"src={editIcon} sx={{'&:hover': { cursor:"pointer", width:"1.5vw"}}}/>
+          </Grid>
+        </Grid>   
       </Grid>
       
         
