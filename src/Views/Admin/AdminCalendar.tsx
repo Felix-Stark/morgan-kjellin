436f81504be7fb1a4ac11c9f00852ee51e954b3b
RickyJohansson <Rickjoh91@hotmail.com>
2023-06-09 15:49:01 +0200

    quick color fix

--- src/Views/Admin/AdminCalendar.tsx
+++ src/Views/Admin/AdminCalendar.tsx
@@ -91,13 +91,13 @@ const AdminCalendar = () => {
             <Box sx={{ backgroundColor: '#342C39', width: { md: '50rem' }, height: '5rem', display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative' }}>
 
                 <Typography variant='h1' color='#FFFFFF' marginTop='2rem'> Kalender </Typography>
-                <InputLabel id='year_label' sx={{ position: 'absolute', right: '8.5rem', top: '3.5rem' }}>År</InputLabel>
+                <InputLabel id='year_label' sx={{ position: 'absolute', right: '8.5rem', top: '3.5rem', color: '#FFFFFF' }}>År</InputLabel>
 
                 <Select
                     labelId='year_label'
                     value={`${selectedYear}`}
                     onChange={ handleYears }
-                    sx={{ position: 'absolute', right: '.5rem', top: '1rem', zIndex: '2', width: '5.5rem', height: '2.5rem', margin: '1.5rem' }}
+                    sx={{ position: 'absolute', right: '.5rem', top: '1rem', zIndex: '2', width: '5.5rem', height: '2.5rem', margin: '1.5rem', color: '#FFFFFF', border: '1px solid #F6F6F6' }}
                     >
                         <MenuItem value={currentYear + 1}>{currentYear + 1}</MenuItem>
                         <MenuItem value={currentYear}>{currentYear}</MenuItem>
